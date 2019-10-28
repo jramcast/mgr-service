ab1665e6f83868d267192d87882194a36b7e9aa5
Jaime Ramírez <jaime.ramirez@roiback.com>
2019-10-28 15:55:24 +0100

    Classification by segments

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from mgr.infrastructure.server.flask import Server
-from mgr.usecases.classify import ClassifyUseCase
+from mgr.usecases.classify import ClassifyUseCase, ClassifySegmentUseCase
 from mgr.infrastructure.models.naivebayes import NaiveBayesModel
 from mgr.infrastructure.youtube import YoutubeAudioLoader
 from flask import request
@@ -16,6 +16,7 @@ audio_downloader = YoutubeAudioLoader()
 
 # Use case initialization
 classify = ClassifyUseCase(models, audio_downloader)
+classify_segment = ClassifySegmentUseCase(models, audio_downloader)
 
 
 # Server and routes defintion
@@ -25,6 +26,14 @@ server = Server()
 @server.route("/classify")
 def route_classify():
     return classify.run(request.args["clip"])
+
+
+@server.route("/segment/classify")
+def route_segment_classify():
+    return classify_segment.run(
+        request.args["clip"],
+        int(request.args["segment"])
+    )
 
 
 # Flask needs to read the app variable to start
--- mgr/usecases/classify.py
+++ mgr/usecases/classify.py
@@ -34,3 +34,56 @@ class ClassifyUseCase:
                 })
 
         return formatted_segments
+
+
+class ClassifySegmentUseCase:
+
+    models: List[Model]
+
+    def __init__(self, models: List[Model], audio_loader: AudioLoader):
+        self.models = models
+        self.audio_loader = audio_loader
+
+    def run(self, uri, segment_index: int) -> List[Any]:
+        clip = self.audio_loader.load(uri)
+        results = {}
+
+        if segment_index >= len(clip.segments):
+            return {
+                "results": {},
+                "segment": segment_index,
+                "nextSegment": None
+            }
+
+        segment = clip.segments[segment_index]
+        # Hack for quick testing
+        segment.segments = [segment]
+
+        for model in self.models:
+            samples = model.preprocess(segment)
+            predictions = model.classify(samples)
+            results[model.name] = self.to_dict(predictions)
+
+        if segment_index < len(clip.segments) - 1:
+            next_segment_index = segment_index + 1
+        else:
+            next_segment_index = None
+
+        return {
+            "results": results,
+            "segment": segment_index,
+            "nextSegment": next_segment_index
+        }
+
+    def to_dict(self, predictions: List[List[Prediction]]):
+        formatted_segments = []
+        for segment in predictions:
+            formatted_segment = []
+            formatted_segments.append(formatted_segment)
+            for prediction in segment:
+                formatted_segment.append({
+                    "label": prediction.label,
+                    "score": prediction.score
+                })
+
+        return formatted_segments
