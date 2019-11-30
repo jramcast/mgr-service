4b02a1254cfa5f294304a05936d432b65e31dc65
Jaime Ramírez <jaime.ram@gmail.com>
2019-11-30 18:16:33 +0100

    Parametrized tf serving urls

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from mgr.infrastructure.server.flask import Server
 from mgr.usecases.classify import ClassifyUseCase
 from mgr.infrastructure.models.naive_bayes.naivebayes import NaiveBayesModel
@@ -11,25 +13,27 @@ from mgr.infrastructure.audioset.vggish.cache.memory import (
 from mgr.infrastructure.audioset.vggish.loader import EmbeddingsLoader
 from flask import request
 
-
-
+# Tensorflow serving urls
+# Models in TF are served separately with TF Serving
+VGGISH_PREDICT_URL = os.environ.get(
+    "VGGISH_PREDICT_URL",
+    "http://vggish:8501/v1/models/vggish:predict")
+FFN_PREDICT_URL = os.environ.get(
+    "FFN_PREDICT_URL",
+    "http://ff_network:8501/v1/models/ff_network:predict")
+RNN_PREDICT_URL = os.environ.get(
+    "RNN_PREDICT_URL",
+    "http://lstm_network:8501/v1/models/lstm_network:predict")
 
 
 features_cache = InMemoryFeaturesCache()
-embeddings_loader = EmbeddingsLoader(
-    "http://vggish:8501/v1/models/vggish:predict",
-    features_cache
-)
+embeddings_loader = EmbeddingsLoader(VGGISH_PREDICT_URL, features_cache)
 
 # Models to classify
 models = [
     NaiveBayesModel(embeddings_loader),
-    FeedForwardNetworkModel(
-        "http://ff_network:8501/v1/models/ff_network:predict",
-        embeddings_loader),
-    LSTMRecurrentNeuralNetwork(
-        "http://lstm_network:8501/v1/models/lstm_network:predict",
-        embeddings_loader),
+    FeedForwardNetworkModel(FFN_PREDICT_URL, embeddings_loader),
+    LSTMRecurrentNeuralNetwork(RNN_PREDICT_URL, embeddings_loader),
     SVMModel(embeddings_loader)
 ]
 
